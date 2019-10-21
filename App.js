e5db855f1bb82d09e010986dd5c5689ffc4ebd9d
herdiiyan <herdiiyan8@gmail.com>
2019-10-22 00:39:13 +0700

    Move Screen to new directory

--- App.js
+++ App.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./src/screen/HomeScreen";
 import Routes from "./src/routes/";
-export default function App() {
-  return <Routes />;
+import { AppLoading } from 'expo';
+import { Container } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+      <Container>
+        <Routes />
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
