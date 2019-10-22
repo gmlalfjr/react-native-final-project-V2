29ab2f7f6237fbcb4f893d9cd46ab7bb522781ca
herdiiyan <herdiiyan8@gmail.com>
2019-10-23 04:02:42 +0700

    Update UI Login

--- src/screen/login/style.js
+++ src/screen/login/style.js
@@ -1,22 +1,42 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      marginTop: "20%",
-      paddingLeft: "10%",
-      paddingRight: "10%"
-    },
-    formInput: {
-      marginLeft: "5%",
-      marginRight: "5%",
-      marginBottom: "10%"
-    },
-    textHeader: {
-      fontSize: 25,
-      paddingBottom: "5%",
-      borderBottomColor: "#7f8c8d",
-      borderBottomWidth: 1,
-      marginBottom: "20%"
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  button: {
+    backgroundColor: 'white',
+    height: 70,
+    marginHorizontal: 20,
+    borderRadius: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 5,
+    shadowOffset: { width: 2, height: 2},
+    shadowColor: 'black',
+    shadowOpacity: 0.2
+  },
+  textInput: {
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 0.5,
+    marginHorizontal: 20,
+    paddingLeft: 10,
+    marginVertical: 5,
+    borderColor: 'rgba(0,0,0,0.2)'
+  },
+  closeButton: {
+    height: 40,
+    width: 40,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: -40,
+    left: "43%"
+  }
   });
   
