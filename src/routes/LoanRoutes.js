f70845289d6ce0e1c7478af839dbccdea2b31403
herdiiyan <herdiiyan8@gmail.com>
2019-10-27 13:00:35 +0700

    Create billing redux

--- src/routes/LoanRoutes.js
+++ src/routes/LoanRoutes.js
@@ -1,10 +1,14 @@
 import { createStackNavigator } from "react-navigation-stack";
 import LoanScreen from "../screen/loan/LoanScreen";
+import BillingStackNavigor from "./BillingRoutes";
 
 const LoanStackNavigor = createStackNavigator(
   {
     Loan: {
       screen: LoanScreen
+    },
+    Billing: {
+      screen: BillingStackNavigor
     }
   },
   {
